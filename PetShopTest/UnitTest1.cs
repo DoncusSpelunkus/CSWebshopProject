9f44e2cd9f233dc9ab0d6629cf48817e14319b58
Kathurjan <91419114+Kathurjan@users.noreply.github.com>
2022-11-24 16:58:01 +0100

    deletetestmethod

--- PetShopTest/UnitTest1.cs
+++ PetShopTest/UnitTest1.cs
@@ -30,4 +30,27 @@ public class UnitTest1
         Assert.Equal(expectedValue, result );
     
     }
+    
+    // test for delete 
+    [Fact]
+    public void DeleteByID_ValidData()
+    {
+
+        Mock<IShopRepo> orderRepository = new Mock<IShopRepo>();
+        int Id = 1;
+        Product product = new Product()
+        {
+            ID = 1, Name = "mockFood1", Price = 10, Description = "very good product1", ImageUrl = "fakeURL1",
+            Rating = 1.5, MainCategory = 0, SubCategory = 0, Brand = 0
+        };
+
+
+        orderRepository.Setup(repo => repo.DeleteProduct(Id)).Returns(product);
+
+        IShopRepo orderService =  orderRepository.Object;
+
+
+        var actual = orderService.DeleteProduct(Id);
+        Assert.Equal(product, actual);
+    }
 }
