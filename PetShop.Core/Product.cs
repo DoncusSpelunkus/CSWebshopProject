d3e582c3dae85fde0562a755ca0f6780f24ec368
Kathurjan <91419114+Kathurjan@users.noreply.github.com>
2022-11-25 13:55:03 +0100

    specs stuff

--- PetShop.Core/Product.cs
+++ PetShop.Core/Product.cs
@@ -15,7 +15,7 @@
          * and an int, which is the ID.
          * 
          */
-        private List<Specs> _specsList { get; set; }
+        //private List<Specs> _specsList { get; set; }
         public int MainCategory { get; set; }
         public int SubCategory { get; set; }
         public int Brand { get; set; }
