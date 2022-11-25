d0b0bf73dca46fc586330c1ae16c73d50f72c577
Kathurjan <91419114+Kathurjan@users.noreply.github.com>
2022-11-25 14:05:18 +0100

    Update IShopRepo.cs

--- PetShop.Application/Interfaces/IShopRepo.cs
+++ PetShop.Application/Interfaces/IShopRepo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Factory.Domain;
+﻿using Factory.Domain;
 
 namespace Factory.Application.Interfaces
 {
