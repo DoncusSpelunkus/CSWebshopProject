0305ffb9edf549b4dd9e8931c31621a83e02d22e
DoncusSpelunkus <90444167+DoncusSpelunkus@users.noreply.github.com>
2022-12-05 17:30:03 +0100

    Did it?

--- PetShop.Frontend/src/app/app.component.ts
+++ PetShop.Frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-
 import {ProductService} from "../services/Product.service";
 import {appRoutingModule} from "./app.router";
 
@@ -10,20 +9,6 @@ import {appRoutingModule} from "./app.router";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  name: string = '';
-  price: number = 0;
-  description: string = '';
-  imageUrl: string = '';
-  rating: number = 0;
-  specs: { [n: number]: string; } | undefined;
-  mainCategory: number = 0;
-  subCategory: number = 0;
-  brand: number = 0;
-  product: any;
-  sname: number;
-  sdesc: string;
-  specList: Array<appValuePair> = [];
-
 
 
   constructor(private http: ProductService) {
@@ -31,36 +16,6 @@ export class AppComponent implements OnInit{
   }
 
   async ngOnInit() {
-    const product = await this.http.GetProduct();
-    this.product = product;
-
   }
 
-  async CreateProduct(){
-    let dto = {
-      name: this.name,
-      price: this.price,
-      description: this.description,
-      imageUrl: this.imageUrl,
-      rating: this.rating,
-      specs: this.specs,
-      mainCategory: this.mainCategory,
-      subCategory: this.subCategory,
-      brand: this.brand,
-      specList: this.specList,
-    }
-    const result = await this.http.CreateProduct(dto);
-    this.product.push(result);
-    this.specList = [];
-  }
-
-  async DeleteProductByID(productID: any) {
-    const product = await this.http.DeleteProductByID(productID);
-    this.product = this.product.filter(p => p.productID != product.productID)
-  }
-
-  async AttachSpecs(spec: number, name: string){
-    let valuePair = new appValuePair(spec,name);
-    this.specList.push(valuePair)
-  }
 }
